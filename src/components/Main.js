92b8bdbacc46980b50f81c843b02a89b43a5c7c8
cyntoia <59616500+cyntoia@users.noreply.github.com>
2020-04-20 22:23:43 -0500

    Update Main.js

--- src/components/Main.js
+++ src/components/Main.js
@@ -104,7 +104,7 @@ class Main extends React.Component {
           style={{ display: 'none' }}
         >
           <h2 className="major">Contact</h2>
-          <form method="post" action="#" data-netlify="true" data-netlify-honeypot="bot-field">>
+          <form method="post" action="#" data-netlify="true" data-netlify-honeypot="bot-field">
           <input type="hidden" name="form-name" value="contact" /> 
             <div className="field half first">
               <label htmlFor="name">Name</label>
